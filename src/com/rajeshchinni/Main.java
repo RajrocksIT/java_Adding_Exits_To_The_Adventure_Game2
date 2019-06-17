3325b559306b5f69de73ad1c3b825f167c42402d
RajrocksIT <chinnirajesh90@gmail.com>
2019-06-17 12:51:35 -0400

    Initial commit

---
+++ src/com/rajeshchinni/Main.java
@@ -0,0 +1,30 @@
+package com.rajeshchinni;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
+        for (String i: road){
+            System.out.println(i);
+        }
+
+        System.out.println("=========================================");
+
+        String[] building = "You are inside a building, a well house for a small spring".split(", ");
+        for (String i: building){
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
